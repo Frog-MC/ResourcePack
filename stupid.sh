3d674e8d10364f4d19796d2c2124e6c881a0b9f9
Cylenia <cylenia@proton.me>
2025-09-26 22:10:36 +0100

    new pack

--- stupid.sh
+++ stupid.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 rm -f release.zip
-zip -r release.zip rp/assets rp/pack.mcmeta
+
+(cd rp && zip -r ../release.zip assets pack.mcmeta)
+
 git add *
 git commit -m "new pack"
 git push
+
